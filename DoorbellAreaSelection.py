f02fcd3f7edd03d62c12df6db35a7ad9622d08cf
Zwk08 <zkessle1@terpmail.umd.edu>
2026-04-30 16:01:02 -0400

    Update DoorbellAreaSelection.py

--- DoorbellAreaSelection.py
+++ DoorbellAreaSelection.py
@@ -4,7 +4,7 @@ import cv2
 
 
 def mask_image(img):
-    mask = np.zeros((img.shape[0],img.shape[1]),dtype = "uint8")
+    #mask = np.zeros((img.shape[0],img.shape[1]),dtype = "uint8")
     for i in range(0,4):
         bbox = cv2.selectROI(img,False)
         print(bbox)
@@ -14,3 +14,4 @@ def mask_image(img):
 test1 = cv2.imread("30-04-2026-15_50_56.jpg")
 cv2.imshow("Original", test1)
 cv2.waitKey(0)
+mask_image(test1)
